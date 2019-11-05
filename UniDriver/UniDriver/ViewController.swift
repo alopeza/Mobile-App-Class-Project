e6c393aff53805c0b43c7db45afd8e6725b2d2a3
Zach <Zachary.garlett@okstate.edu>
2019-11-05 14:32:51 -0600

    Project Set up
    The first thing I added was custom font 'Gibson' it was the font used in one of the UI mock-ups.

---
+++ UniDriver/UniDriver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniDriver
+//
+//  Created by Zachary Garlett on 10/18/19.
+//  Copyright © 2019 Zachary Garlett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
