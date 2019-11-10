eb336383c67df47f88e35d6fc3b5d42030e39b30
Zach <Zachary.garlett@okstate.edu>
2019-11-09 19:56:33 -0600

    Updated back button

--- UniDriver/UniDriver/ViewController.swift
+++ UniDriver/UniDriver/ViewController.swift
@@ -15,10 +15,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    //Programmatically go back in the navigation controller with animation
     @IBAction func back(_ sender: Any) {
         _ = navigationController?.popViewController(animated: true)
     }
     
-    
+    //Programmatically go back in the navigation controller without animation
+    @IBAction func backButton(_ sender: Any) {
+        _ = navigationController?.popViewController(animated: false)
+    }
 }
 
