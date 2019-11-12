106e2b81693aa21df1759c0f19d93df2a4a840f9
Zach <Zachary.garlett@okstate.edu>
2019-11-11 20:11:26 -0600

    Outlets for Sign in and Sign up

---
+++ UniDriver/UniDriver/RiderSignUpController.swift
@@ -0,0 +1,42 @@
+//
+//  RiderSignUpController.swift
+//  UniDriver
+//
+//  Created by Zachary Garlett on 11/11/19.
+//  Copyright © 2019 Zachary Garlett. All rights reserved.
+//
+
+import UIKit
+
+class RiderSignUpController: UIViewController {
+
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var nameOfCard: UITextField!
+    @IBOutlet weak var cardNumber: UITextField!
+    @IBOutlet weak var expDate: UITextField!
+    @IBOutlet weak var cvv: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func back(_ sender: Any) {
+         _ = navigationController?.popViewController(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
