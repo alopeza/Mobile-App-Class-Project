d345f1c977af74808a12124dfc3b6e799b8da6ef
Allan Nguyen <allan.nguyen@okstate.edu>
2019-11-13 02:00:34 -0600

    Initial Commit

---
+++ UniDriver/LoginController.swift
@@ -0,0 +1,36 @@
+//
+//  loginController.swift
+//  UniDriver
+//
+
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func back(_ sender: Any) {
+        _ = navigationController?.popToRootViewController(animated: true)
+            
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UniDriver/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UniDriver
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    //Programmatically go back in the navigation controller with animation
+    @IBAction func back(_ sender: Any) {
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+    //Programmatically go back in the navigation controller without animation
+    @IBAction func backButton(_ sender: Any) {
+        _ = navigationController?.popViewController(animated: false)
+    }
+}
+
