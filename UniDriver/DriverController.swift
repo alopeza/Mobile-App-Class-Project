5e625430aa3459da7c8c80d487d1468b04ca5137
Zach <Zachary.garlett@okstate.edu>
2019-11-23 16:13:17 -0600

    Removed gesture

--- UniDriver/DriverController.swift
+++ UniDriver/DriverController.swift
@@ -24,8 +24,7 @@ class DriverController: UIViewController {
         clockOutButton.isHidden = true;
         clockOutButton.isEnabled = false;
         
-        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
-        view.addGestureRecognizer(tap)
+        
     }
     @IBOutlet weak var DriverMapView: GMSMapView!
     
